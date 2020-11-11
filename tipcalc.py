06af13cace24e315d09b9485cee98b16f51aa597
Michael Cook <michaelcookv2.0@gmail.com>
2020-11-10 23:07:26 -0500

    no idea oe to do this

---
+++ tipcalc.py
@@ -0,0 +1,9 @@
+meal = int(input ("Enter your meal amount: "))
+service_level = (input ("How was your service? ")
+
+if service_level = (input (good)
+
+
+total_bill = meal + service_level
+
+print(f"Your bill is ${} and your service was (service_level)" )
