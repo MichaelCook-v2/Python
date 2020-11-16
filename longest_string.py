de9296ec245aaa6fe66f66599b3b1fb28fbcb5c7
Michael Cook <michaelcookv2.0@gmail.com>
2020-11-15 22:58:50 -0500

    Labs

--- longest_string.py
+++ longest_string.py
@@ -1,8 +1,8 @@
-longest_list = int(1,12,3,452,84,10,8956,19,132,745,14523625,7845632154,42,98)
+longest_list = "1,12,3,452,84,10,8956,19,132,745,14523625,7845632154,42,98"
 print("The original list" + str(longest_list))
 
 longest_list = -1
-for ele in longest_list:
+for ele int in longest_list:
     if len(ele) > longest_list:
         longest_list = len(ele)
         res = ele
