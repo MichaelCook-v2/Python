bd00c468381333b016c3d1d4c93123986e43281c
Michael Cook <michaelcookv2.0@gmail.com>
2020-11-16 14:09:23 -0500

    monday labs from last week

---
+++ largest_num.py
@@ -0,0 +1,2 @@
+longest_list=12,3,452,84,10,8956,19,132,745,14523625,7845632154,42,98
+print("Largest number in the list is:", max(longest_list))
--- longest_string.py
+++
@@ -1,9 +0,0 @@
-longest_list = "1,12,3,452,84,10,8956,19,132,745,14523625,7845632154,42,98"
-print("The original list" + str(longest_list))
-
-longest_list = -1
-for ele int in longest_list:
-    if len(ele) > longest_list:
-        longest_list = len(ele)
-        res = ele
-print("Largest number is: " + res)
