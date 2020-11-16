704fc561ba6e505df2d50701e9eaab008005efaf
Michael Cook <michaelcookv2.0@gmail.com>
2020-11-15 19:41:37 -0500

    Friday homework

---
+++ mad_lib.py
@@ -0,0 +1,7 @@
+
+name = input("Enter a name: ")
+name2 = input ("Enter another name:")
+fav_subject = input("Enter your favorite subject: ")
+fav_subject2 = input("Enter another favorite subject:")
+
+print(name + "'s favorite subject is"  + ,fav_subject "and" + ,name2 "favorite subject is " + ,fav_subject2)
---
+++ smallest_num.py
@@ -0,0 +1,2 @@
+smallest =(345,87,12,35,88,9,55,32,105,1005,26,5)
+print("Smallest number in the list is:", min(smallest))
